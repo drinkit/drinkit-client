6392619c121f49338c5a0f835748713fdaf26e55
pkolmykov <pavel.kolmykov@gmail.com>
2013-07-25 01:36:24 +0300

    refactoring

--- server/src/main/java/ua/kiev/naiv/drinkit/cocktail/search/CriteriaPOJO.java
+++ server/src/main/java/ua/kiev/naiv/drinkit/cocktail/search/CriteriaPOJO.java
@@ -1,8 +1,6 @@
 package ua.kiev.naiv.drinkit.cocktail.search;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import ua.kiev.naiv.drinkit.cocktail.model.CocktailType;
-import ua.kiev.naiv.drinkit.cocktail.model.Ingredient;
 
 import java.util.Set;
 
@@ -14,18 +12,18 @@ import java.util.Set;
  */
 public class CriteriaPOJO {
 
-    private Set<CocktailType> cocktailTypes;
+    private Set<Integer> cocktailTypes;
     private Boolean isBurning;
     private Boolean isWithIce;
     private Boolean isChecked;
     private Boolean isIBA;
     private Boolean isFlacky;
-    private Set<Ingredient> ingredients;
+    private Set<Integer> ingredients;
 
     public CriteriaPOJO() {
     }
 
-    public CriteriaPOJO(Set<CocktailType> cocktailTypes, Boolean burning, Boolean withIce, Boolean checked, Boolean IBA, Boolean flacky, Set<Ingredient> ingredients) {
+    public CriteriaPOJO(Set<Integer> cocktailTypes, Boolean burning, Boolean withIce, Boolean checked, Boolean IBA, Boolean flacky, Set<Integer> ingredients) {
         this.cocktailTypes = cocktailTypes;
         isBurning = burning;
         isWithIce = withIce;
@@ -36,11 +34,11 @@ public class CriteriaPOJO {
     }
 
     @JsonIdentityReference(alwaysAsId = true)
-    public Set<CocktailType> getCocktailTypes() {
+    public Set<Integer> getCocktailTypes() {
         return cocktailTypes;
     }
 
-    public void setCocktailTypes(Set<CocktailType> cocktailTypes) {
+    public void setCocktailTypes(Set<Integer> cocktailTypes) {
         this.cocktailTypes = cocktailTypes;
     }
 
@@ -85,41 +83,42 @@ public class CriteriaPOJO {
     }
 
     @JsonIdentityReference(alwaysAsId = true)
-    public Set<Ingredient> getIngredients() {
+    public Set<Integer> getIngredients() {
         return ingredients;
     }
 
-    public void setIngredients(Set<Ingredient> ingredients) {
+    public void setIngredients(Set<Integer> ingredients) {
         this.ingredients = ingredients;
     }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        CriteriaPOJO that = (CriteriaPOJO) o;
-//
-//        if (!cocktailTypes.equals(that.cocktailTypes)) return false;
-//        if (!ingredients.equals(that.ingredients)) return false;
-//        if (!isBurning.equals(that.isBurning)) return false;
-//        if (!isChecked.equals(that.isChecked)) return false;
-//        if (!isFlacky.equals(that.isFlacky)) return false;
-//        if (!isIBA.equals(that.isIBA)) return false;
-//        if (!isWithIce.equals(that.isWithIce)) return false;
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result = cocktailTypes.hashCode();
-//        result = 31 * result + isBurning.hashCode();
-//        result = 31 * result + isWithIce.hashCode();
-//        result = 31 * result + isChecked.hashCode();
-//        result = 31 * result + isIBA.hashCode();
-//        result = 31 * result + isFlacky.hashCode();
-//        result = 31 * result + ingredients.hashCode();
-//        return result;
-//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CriteriaPOJO that = (CriteriaPOJO) o;
+
+        if (cocktailTypes != null ? !cocktailTypes.equals(that.cocktailTypes) : that.cocktailTypes != null)
+            return false;
+        if (!ingredients.equals(that.ingredients)) return false;
+        if (isBurning != null ? !isBurning.equals(that.isBurning) : that.isBurning != null) return false;
+        if (isChecked != null ? !isChecked.equals(that.isChecked) : that.isChecked != null) return false;
+        if (isFlacky != null ? !isFlacky.equals(that.isFlacky) : that.isFlacky != null) return false;
+        if (isIBA != null ? !isIBA.equals(that.isIBA) : that.isIBA != null) return false;
+        if (isWithIce != null ? !isWithIce.equals(that.isWithIce) : that.isWithIce != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cocktailTypes != null ? cocktailTypes.hashCode() : 0;
+        result = 31 * result + (isBurning != null ? isBurning.hashCode() : 0);
+        result = 31 * result + (isWithIce != null ? isWithIce.hashCode() : 0);
+        result = 31 * result + (isChecked != null ? isChecked.hashCode() : 0);
+        result = 31 * result + (isIBA != null ? isIBA.hashCode() : 0);
+        result = 31 * result + (isFlacky != null ? isFlacky.hashCode() : 0);
+        result = 31 * result + ingredients.hashCode();
+        return result;
+    }
 }
--- server/src/test/java/ua/kiev/naiv/drinkit/cocktail/model/SearchTest.java
+++ server/src/test/java/ua/kiev/naiv/drinkit/cocktail/model/SearchTest.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.kiev.naiv.drinkit.cocktail.search.CriteriaPOJO;
-import ua.kiev.naiv.drinkit.cocktail.service.CocktailService;
-import ua.kiev.naiv.drinkit.cocktail.service.IngredientService;
 import ua.kiev.naiv.drinkit.springconfig.TestConfig;
 
 import java.io.File;
@@ -26,32 +23,25 @@ import java.util.HashSet;
 @ContextConfiguration(classes = TestConfig.class)
 public class SearchTest {
 
-    @Autowired
-    IngredientService ingredientService;
-
-    @Autowired
-    CocktailService cocktailService;
-
     private CriteriaPOJO getMockCriteria() {
         CriteriaPOJO criteria = new CriteriaPOJO();
         criteria.setBurning(true);
         criteria.setChecked(false);
         criteria.setIBA(false);
         criteria.setFlacky(true);
-        criteria.setIngredients(new HashSet<Ingredient>() {{
-            add(ingredientService.getById(1));
-            add(ingredientService.getById(2));
+        criteria.setIngredients(new HashSet<Integer>() {{
+            add(1);
+            add(2);
         }});
-        criteria.setCocktailTypes(new HashSet<CocktailType>() {{
-            add(cocktailService.getCocktailType(1));
-            add(cocktailService.getCocktailType(2));
+        criteria.setCocktailTypes(new HashSet<Integer>() {{
+            add(1);
+            add(2);
         }});
         return criteria;
     }
 
     @Test
     public void jsonTest() throws IOException {
-        Cocktail cubaLibre = cocktailService.findById(1);
         File tmpFile = new File("d:\\tmp\\json.json");
         ObjectMapper objectMapper = new ObjectMapper();
         CriteriaPOJO mockCriteria = getMockCriteria();
