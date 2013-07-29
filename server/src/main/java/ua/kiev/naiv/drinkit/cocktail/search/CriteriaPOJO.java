d1ed40a570bfbb9e71a98b25f3a5ea0c2de95f01
pkolmykov <pavel.kolmykov@gmail.com>
2013-07-29 18:26:40 +0300

    criteria deserialization

--- server/src/main/java/ua/kiev/naiv/drinkit/cocktail/search/CriteriaPOJO.java
+++ server/src/main/java/ua/kiev/naiv/drinkit/cocktail/search/CriteriaPOJO.java
@@ -1,7 +1,5 @@
 package ua.kiev.naiv.drinkit.cocktail.search;
 
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-
 import java.util.Set;
 
 /**
@@ -13,27 +11,13 @@ import java.util.Set;
 public class CriteriaPOJO {
 
     private Set<Integer> cocktailTypes;
-    private Boolean isBurning;
-    private Boolean isWithIce;
-    private Boolean isChecked;
-    private Boolean isIBA;
-    private Boolean isFlacky;
     private Set<Integer> ingredients;
+    private Set<Integer> options;
 
     public CriteriaPOJO() {
     }
 
-    public CriteriaPOJO(Set<Integer> cocktailTypes, Boolean burning, Boolean withIce, Boolean checked, Boolean IBA, Boolean flacky, Set<Integer> ingredients) {
-        this.cocktailTypes = cocktailTypes;
-        isBurning = burning;
-        isWithIce = withIce;
-        isChecked = checked;
-        isIBA = IBA;
-        isFlacky = flacky;
-        this.ingredients = ingredients;
-    }
-
-    @JsonIdentityReference(alwaysAsId = true)
+    //    @JsonIdentityReference(alwaysAsId = true)
     public Set<Integer> getCocktailTypes() {
         return cocktailTypes;
     }
@@ -42,47 +26,7 @@ public class CriteriaPOJO {
         this.cocktailTypes = cocktailTypes;
     }
 
-    public Boolean getBurning() {
-        return isBurning;
-    }
-
-    public void setBurning(Boolean burning) {
-        isBurning = burning;
-    }
-
-    public Boolean getWithIce() {
-        return isWithIce;
-    }
-
-    public void setWithIce(Boolean withIce) {
-        isWithIce = withIce;
-    }
-
-    public Boolean getChecked() {
-        return isChecked;
-    }
-
-    public void setChecked(Boolean checked) {
-        isChecked = checked;
-    }
-
-    public Boolean getIBA() {
-        return isIBA;
-    }
-
-    public void setIBA(Boolean IBA) {
-        isIBA = IBA;
-    }
-
-    public Boolean getFlacky() {
-        return isFlacky;
-    }
-
-    public void setFlacky(Boolean flacky) {
-        isFlacky = flacky;
-    }
-
-    @JsonIdentityReference(alwaysAsId = true)
+    //    @JsonIdentityReference(alwaysAsId = true)
     public Set<Integer> getIngredients() {
         return ingredients;
     }
@@ -91,34 +35,11 @@ public class CriteriaPOJO {
         this.ingredients = ingredients;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CriteriaPOJO that = (CriteriaPOJO) o;
-
-        if (cocktailTypes != null ? !cocktailTypes.equals(that.cocktailTypes) : that.cocktailTypes != null)
-            return false;
-        if (!ingredients.equals(that.ingredients)) return false;
-        if (isBurning != null ? !isBurning.equals(that.isBurning) : that.isBurning != null) return false;
-        if (isChecked != null ? !isChecked.equals(that.isChecked) : that.isChecked != null) return false;
-        if (isFlacky != null ? !isFlacky.equals(that.isFlacky) : that.isFlacky != null) return false;
-        if (isIBA != null ? !isIBA.equals(that.isIBA) : that.isIBA != null) return false;
-        if (isWithIce != null ? !isWithIce.equals(that.isWithIce) : that.isWithIce != null) return false;
-
-        return true;
+    public Set<Integer> getOptions() {
+        return options;
     }
 
-    @Override
-    public int hashCode() {
-        int result = cocktailTypes != null ? cocktailTypes.hashCode() : 0;
-        result = 31 * result + (isBurning != null ? isBurning.hashCode() : 0);
-        result = 31 * result + (isWithIce != null ? isWithIce.hashCode() : 0);
-        result = 31 * result + (isChecked != null ? isChecked.hashCode() : 0);
-        result = 31 * result + (isIBA != null ? isIBA.hashCode() : 0);
-        result = 31 * result + (isFlacky != null ? isFlacky.hashCode() : 0);
-        result = 31 * result + ingredients.hashCode();
-        return result;
+    public void setOptions(Set<Integer> options) {
+        this.options = options;
     }
 }
--- server/src/test/java/ua/kiev/naiv/drinkit/cocktail/model/SearchTest.java
+++ server/src/test/java/ua/kiev/naiv/drinkit/cocktail/model/SearchTest.java
@@ -2,17 +2,11 @@ package ua.kiev.naiv.drinkit.cocktail.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import junit.framework.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.kiev.naiv.drinkit.cocktail.search.CriteriaPOJO;
-import ua.kiev.naiv.drinkit.springconfig.TestConfig;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,35 +14,16 @@ import java.util.HashSet;
  * Date: 22.07.13
  * Time: 21:41
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = TestConfig.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = TestConfig.class)
 public class SearchTest {
 
-    private CriteriaPOJO getMockCriteria() {
-        CriteriaPOJO criteria = new CriteriaPOJO();
-        criteria.setBurning(true);
-        criteria.setChecked(false);
-        criteria.setIBA(false);
-        criteria.setFlacky(true);
-        criteria.setIngredients(new HashSet<Integer>() {{
-            add(1);
-            add(2);
-        }});
-        criteria.setCocktailTypes(new HashSet<Integer>() {{
-            add(1);
-            add(2);
-        }});
-        return criteria;
-    }
+    private final static String CRITERIA_JSON_PATH = "json/request/search.json";
 
-    @Ignore
     @Test
-    public void jsonTest() throws IOException {
-        File tmpFile = new File("d:\\tmp\\json.json");
+    public void readAndSerializeJsonIntoCriteria() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        CriteriaPOJO mockCriteria = getMockCriteria();
-        objectMapper.writeValue(tmpFile, mockCriteria);
-        CriteriaPOJO criteriaPOJO = objectMapper.readValue(tmpFile, CriteriaPOJO.class);
-        Assert.assertEquals(criteriaPOJO, mockCriteria);
+        CriteriaPOJO criteria = objectMapper.readValue(new File(CRITERIA_JSON_PATH), CriteriaPOJO.class);
+        Assert.assertNotNull(criteria);
     }
 }
