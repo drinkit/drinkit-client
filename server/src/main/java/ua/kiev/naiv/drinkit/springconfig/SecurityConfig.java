d3359f15348c6fbbb54e77e6651e931e7e2986d2
pkolmykov <pavel.kolmykov@gmail.com>
2013-11-07 17:06:43 +0200

    Basic rest spring security config

--- server/src/main/java/ua/kiev/naiv/drinkit/springconfig/SecurityConfig.java
+++ server/src/main/java/ua/kiev/naiv/drinkit/springconfig/SecurityConfig.java
@@ -2,15 +2,19 @@ package ua.kiev.naiv.drinkit.springconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ua.kiev.naiv.drinkit.security.AuthenticationTokenProcessingFilter;
 
 /**
  * @author pkolmykov
  */
 @Configuration
 @EnableWebSecurity()
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
@@ -18,4 +22,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         auth.inMemoryAuthentication().withUser("user").password("pwd").roles("user");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .antMatcher("/rest/**")
+                .addFilterAfter(new AuthenticationTokenProcessingFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
+    }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.
+//    }
 }
