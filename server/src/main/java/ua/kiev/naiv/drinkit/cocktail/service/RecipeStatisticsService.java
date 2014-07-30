c4d004a69a5505f948f068b456214176a363c440
pkolmykov <pavel.kolmykov@gmail.com>
2014-07-30 20:04:46 +0300

    fix compilations error

--- server/src/main/java/ua/kiev/naiv/drinkit/cocktail/service/RecipeStatisticsService.java
+++
@@ -1,7 +0,0 @@
-package ua.kiev.naiv.drinkit.cocktail.service;
-
-public interface RecipeStatisticsService {
-    void incrementViewsCount(int recipeId, int userId);
-
-    void setRecipeRatingForUser(int id, int userId, int i);
-}
