8294c8a3167d17c3c406dcef291512939e7850b7
pkolmykov <pavel.kolmykov@gmail.com>
2014-09-28 14:42:45 +0300

    changed scrips (cherry picked from commit 0c7a599)

--- server/src/main/resources/db/migration/V1_1__Adding_recipes_statistics.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `recipe_statistics` (
-  `recipe_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `views` int(11) NOT NULL DEFAULT '0',
-  `rating` tinyint(1) DEFAULT NULL,
-  `last_timestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`recipe_id`, `user_id`),
-  CONSTRAINT `statistics_fk_recipes` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- server/src/main/resources/db/migration/V1_2__Adding_admins_users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO dev.users (username, displayName, password, accessLevel) VALUES ('crabar0@gmail.com', 'Crabar', 'bec46b7ba3126e3a5e5884d363f898ba5f0221b33b0088b7eec37360d2f183a2', '0');
-INSERT INTO dev.users (username, displayName, password, accessLevel) VALUES ('pavel.kolmykov@gmail.com', 'Naiv', 'bec46b7ba3126e3a5e5884d363f898ba5f0221b33b0088b7eec37360d2f183a2', '0');
