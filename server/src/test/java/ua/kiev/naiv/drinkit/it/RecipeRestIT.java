7c176d9070755932ba30a952f334afd1c7ca03b7
pkolmykov <pavel.kolmykov@gmail.com>
2014-10-19 19:31:24 +0300

    fixed it tests

--- server/src/test/java/ua/kiev/naiv/drinkit/it/RecipeRestIT.java
+++ server/src/test/java/ua/kiev/naiv/drinkit/it/RecipeRestIT.java
@@ -1,12 +1,14 @@
 package ua.kiev.naiv.drinkit.it;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import ua.kiev.naiv.drinkit.cocktail.persistence.model.Ingredient;
+import ua.kiev.naiv.drinkit.cocktail.service.IngredientService;
 import ua.kiev.naiv.drinkit.cocktail.service.RecipeService;
 import ua.kiev.naiv.drinkit.cocktail.web.model.Recipe;
 import ua.kiev.naiv.drinkit.springconfig.AppConfig;
@@ -15,12 +17,14 @@ import javax.persistence.EntityManagerFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static ua.kiev.naiv.drinkit.MockObjectsGenerator.creteMockRecipe;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 @Transactional()
 public class RecipeRestIT {
+
+    private Ingredient firstIngredient = new Ingredient();
+    private Ingredient secondIngredient = new Ingredient();
 
     @Autowired
     EntityManagerFactory entityManagerFactory;
@@ -28,16 +32,35 @@ public class RecipeRestIT {
     @Autowired
     RecipeService recipeService;
 
+    @Autowired
+    IngredientService ingredientService;
+
+    @Before
+    public void initTestData() {
+        System.out.println("before");
+        firstIngredient.setDescription("firstIngredient");
+        firstIngredient.setName("First");
+        firstIngredient.setVol(30);
+        ingredientService.create(firstIngredient);
+
+        secondIngredient.setDescription("secondIngredient");
+        secondIngredient.setName("Second");
+        secondIngredient.setVol(40);
+        ingredientService.create(secondIngredient);
+
+    }
+
     @Test
-    @Rollback(false)
+//    @Rollback(false)
     public void recipeTestCase() {
-        Recipe recipe = creteMockRecipe();
-        int id = recipeService.save(recipe).getId();
-        assertEquals(recipe, recipeService.getRecipeById(id));
-        recipe.setName("modified");
-        recipe.setId(id);
-        recipeService.save(recipe);
-        assertEquals(recipe, recipeService.getRecipeById(id));
+        Recipe created = recipeService.save(createNewRecipe());
+        int id = created.getId();
+        Recipe modifiedRecipe = recipeService.getRecipeById(id);
+        assertEquals(created, modifiedRecipe);
+        modifiedRecipe.setName("modified");
+        modifiedRecipe.setId(id);
+        recipeService.save(modifiedRecipe);
+        assertEquals(modifiedRecipe, recipeService.getRecipeById(id));
         recipeService.delete(id);
         assertNull(recipeService.getRecipeById(id));
     }
@@ -45,13 +68,23 @@ public class RecipeRestIT {
     @Test
     public void recipeStatisticsTest() {
         int userId = -1;
-        Recipe createdRecipe = recipeService.save(creteMockRecipe());
+        Recipe createdRecipe = recipeService.save(createNewRecipe());
         assertEquals(0, createdRecipe.getViews());
         assertNull(createdRecipe.getRating());
 //        recipeStatisticsService.setRecipeRatingForUser(recipe.getId(), userId, 6);//todo
         Recipe receivedRecipe = recipeService.getRecipeByIdAndIncrementViewsCount(createdRecipe.getId(), userId);
         assertEquals(1, receivedRecipe.getViews());
         assertNull(receivedRecipe.getRating());
+    }
+
+    private Recipe createNewRecipe() {
+        Recipe recipe = new Recipe();
+        recipe.setCocktailTypeId(1);
+        recipe.setDescription("desc");
+        recipe.setName("Test2");
+        recipe.setOptions(new int[]{1, 2});
+        recipe.setCocktailIngredients(new Integer[][]{{firstIngredient.getId(), 50}, {secondIngredient.getId(), 60}});
+        return recipe;
     }
 
 }
